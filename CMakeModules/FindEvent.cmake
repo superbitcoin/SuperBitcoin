e7556b4a73abc40a27b3c1dacc108f9810541501
Ben002 <bencgping@gmail.com>
2017-12-28 22:10:05 +0800

    add cmake list

---
+++ CMakeModules/FindEvent.cmake
@@ -0,0 +1,49 @@
+# Find event
+#
+# Find the event includes and library
+#
+# if you nee to add a custom library search path, do it via CMAKE_PREFIX_PATH
+#
+# This module defines
+#  EVENT_INCLUDE_DIRS, where to find header, etc.
+#  EVENT_LIBRARIES, the libraries needed to use event.
+#  EVENT_FOUND, If false, do not try to use event.
+
+# only look in default directories
+find_path(
+	EVENT_INCLUDE_DIR
+	NAMES event2/event.h
+	DOC "event include dir"
+)
+
+find_library(
+	EVENT_LIBRARY
+	NAMES event
+	DOC "event library"
+)
+
+set(EVENT_INCLUDE_DIRS ${EVENT_INCLUDE_DIR})
+set(EVENT_LIBRARIES ${EVENT_LIBRARY})
+
+# debug library on windows
+# same naming convention as in QT (appending debug library with d)
+# boost is using the same "hack" as us with "optimized" and "debug"
+if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "MSVC")
+
+	find_library(
+		EVENT_LIBRARY_DEBUG
+		NAMES eventd
+		DOC "event debug library"
+	)
+
+	set(EVENT_LIBRARIES "iphlpapi" optimized ${EVENT_LIBRARIES} debug ${EVENT_LIBRARY_DEBUG})
+
+endif()
+
+# handle the QUIETLY and REQUIRED arguments and set EVENT_FOUND to TRUE
+# if all listed variables are TRUE, hide their existence from configuration view
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(event DEFAULT_MSG
+	EVENT_LIBRARY EVENT_INCLUDE_DIR)
+mark_as_advanced (EVENT_INCLUDE_DIR EVENT_LIBRARY)
+
