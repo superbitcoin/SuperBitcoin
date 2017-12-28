dc6c0c83a4ed0edfd291a78762c85884fe46ce4c
ben <bencgping@gmail.com>
2017-12-28 22:31:05 +0800

    add cmake list

--- src/leveldb/port/port.h
+++ src/leveldb/port/port.h
@@ -10,6 +10,7 @@
 // Include the appropriate platform specific file below.  If you are
 // porting to a new platform, see "port_example.h" for documentation
 // of what the new port_<platform>.h file must provide.
+#  include "port/port_posix.h"
 #if defined(LEVELDB_PLATFORM_POSIX)
 #  include "port/port_posix.h"
 #elif defined(LEVELDB_PLATFORM_CHROMIUM)
