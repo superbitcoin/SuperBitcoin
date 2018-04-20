61ff133f3c5c6d8863ecb9c0c4d8ca53b2ff9f0f
bille18 <flying_why17@outlook.com>
2018-04-20 21:58:02 +0800

    fix the GetState param

--- src/contract-api/contractcomponent.cpp
+++ src/contract-api/contractcomponent.cpp
@@ -846,7 +846,7 @@ bool CContractComponent::ContractTxConnectBlock(CTransaction tx, uint32_t transa
     return true;
 }
 
-void CContractComponent::GetState(uint256 &hashStateRoot, uint256 &hashUTXORoo)
+void CContractComponent::GetState(uint256 &hashStateRoot, uint256 &hashUTXORoot)
 {
     GET_CHAIN_INTERFACE(ifChainObj);
     if (!ifChainObj->IsSBTCContractEnabled(ifChainObj->GetActiveChain().Tip()))
