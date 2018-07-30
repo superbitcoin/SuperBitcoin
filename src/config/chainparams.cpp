badca85bfe44ee2c2e3044e847831afd5810c3cb
ranger.shi <ranger.shi@gmail.com>
2018-07-30 17:02:40 +0800

    add the smart  contranct fork id

--- src/config/chainparams.cpp
+++ src/config/chainparams.cpp
@@ -131,7 +131,7 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1479168000; // November 15th, 2016.
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 1510704000; // November 15th, 2017.
 
-        consensus.SBTCContractForkHeight = 530555 - 1; // 525555 is the first new block height
+        consensus.SBTCContractForkHeight = 531764 - 1; // 531763 is the first new block height
         consensus.vDeployments[Consensus::DEPLOYMENT_SBTC_CONTRACT].bit = VERSIONBITS_SBTC_CONTRACT;
 
         // The best chain should have at least this much work.
